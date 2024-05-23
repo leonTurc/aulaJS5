85ee89833c9f01b4882643373559904d1599e41b
Leonardo Turczinski <turczinskileon@gmail.com>
2024-05-23 17:50:02 -0300

    commit1

---
+++ js/aula5.js
@@ -0,0 +1,17 @@
+var idd;
+
+idd= prompt("insira a idade do nadador")
+
+if(idd >=5 && idd <=7){
+    alert("nadador infantil")
+} else if(idd >=8 && idd <=10){
+    alert("nadador infantil")
+} else if(idd >=11 && idd <=13){
+    alert("nadador juvenil")
+} else if(idd >=14 && idd <=17){
+    alert("nadador juvenil")
+} else if (idd >= 18){
+    alert("nadador adulto")
+} else{
+    alert("nadador inapto")
+}
